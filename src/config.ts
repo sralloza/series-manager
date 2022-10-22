dd86c1429b6861acc41afaa58fccf39e8eee5187
Diego Alloza González <diego.allozagonzalez@telefonica.com>
2022-10-22 20:11:03 +0200

    feat: set config_path from env variable

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,8 @@ import { Config } from './models/config';
 
 dotenv.config();
 
-const config: Config = JSON.parse(readFileSync('config.json').toString());
+const configPath = process.env.CONFIG_PATH || './config.json';
+const config: Config = JSON.parse(readFileSync(configPath).toString());
 const token = process.env.TODOIST_API_TOKEN;
 if (!token) {
   throw new Error('TODOIST_API_TOKEN not set');
